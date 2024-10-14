32f58686248ac1c4e45626df14fb052a6e6cfc6e
santiserena <santiserena@outlook.com.ar>
2024-10-14 20:55:11 -0300

    images showed

---
+++ caceres-art-portfolio-proyect/src/Data.js
@@ -0,0 +1,20 @@
+// data.js
+const data = [
+  {
+    id: 1,
+    name: "Emiliano",
+    url: "https://i.pinimg.com/236x/5f/bd/1a/5fbd1acf65f6aa5eaae7d0fb432c8957.jpg",
+  },
+  {
+    id: 2,
+    name: "Santiago",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRodnSl27AAz4Sqrct_vFzn6mAPM1IEdErX_Q&s",
+  },
+  {
+    id: 3,
+    name: "Lucía",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrCpkuv-s7nsn_EY9X8O_0fSV-sfeqx54ncA&s",
+  },
+];
+
+export default data;
--- caceres-art-portfolio-proyect/src/pages/Home.jsx
+++ caceres-art-portfolio-proyect/src/pages/Home.jsx
@@ -1,11 +1,27 @@
+import React, { useEffect } from "react";
 import Button from "./HomeStyles";
+import data from "../Data";
 
 function Home() {
+  useEffect(() => {
+    console.log(data);
+  }, []);
+
   return (
     <div>
-      <h1>Portfolio de Emiliano C.</h1>;
+      <h1>Portfolio de Emiliano C.</h1>
+      {data.map((item, index) => (
+        <img
+          key={index}
+          src={item.url}
+          alt={item.name}
+          /* borrar style */
+          style={{ width: "100px", height: "auto", margin: "10px" }} // Estilo para tamaño pequeño
+        />
+      ))}
       <Button />
     </div>
   );
 }
+
 export default Home;

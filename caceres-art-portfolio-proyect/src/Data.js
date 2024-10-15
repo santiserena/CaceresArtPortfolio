6b24001308bec42e3cbc88ab95b3bec4c8bf067a
santiserena <santiserena@outlook.com.ar>
2024-10-14 21:02:46 -0300

    images added

--- caceres-art-portfolio-proyect/src/Data.js
+++ caceres-art-portfolio-proyect/src/Data.js
@@ -2,18 +2,33 @@
 const data = [
   {
     id: 1,
-    name: "Emiliano",
+    name: "dibujo1",
     url: "https://i.pinimg.com/236x/5f/bd/1a/5fbd1acf65f6aa5eaae7d0fb432c8957.jpg",
   },
   {
     id: 2,
-    name: "Santiago",
+    name: "dibujo2",
     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRodnSl27AAz4Sqrct_vFzn6mAPM1IEdErX_Q&s",
   },
   {
     id: 3,
-    name: "Lucía",
+    name: "dibujo3",
     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrCpkuv-s7nsn_EY9X8O_0fSV-sfeqx54ncA&s",
+  },
+  {
+    id: 4,
+    name: "dibujo4",
+    url: "https://images.cdn2.buscalibre.com/fit-in/360x360/2e/9e/2e9ef3597f6c36fb4369fded8f3ddfd5.jpg",
+  },
+  {
+    id: 5,
+    name: "dibujo5",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYoEL4zqm_SOFfht0jVLTQ_UtBMF0eVEejzA&s",
+  },
+  {
+    id: 6,
+    name: "dibujo6",
+    url: "https://i0.wp.com/videojuegos.enriqueortegaburgos.com/wp-content/uploads/2022/06/TIMELY-COMICS.jpg?fit=1024%2C683&ssl=1",
   },
 ];
 
